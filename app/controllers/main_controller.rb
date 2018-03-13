62c5bbc7089b9482d967aeee25cfeb71fd10a594
juanmamaffei <juanmamaffei@bitbucket.com>
2018-03-13 07:44:54 -0300

    Routeo de logueados

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,21 @@
 class MainController < ApplicationController
+	#before_action :authenticate_user!, except(:show)
 	layout false
+
+	def index
+		set_cards
+		set_companies
+	end
+
+	def landing
+	end
+	
+	private
+		def set_cards
+			@cards = Card.where(user: current_user).order(created_at: :desc)
+			@company_names = Company.select("name")
+		end
+		def set_companies
+			@companies = Company.where(admin: current_user).order(created_at: :desc)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
   resources :companies do
   	resources :cards
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'main#index'
+  
+  authenticated :user do
+  	root 'main#index'
+  end
+  unauthenticated :user do
+  	devise_scope :user do
+  		root 'main#landing'
+  	end
+  end
 
 end
