416f9c38d67134dde56822cca94a1560acc398af
juanmamaffei <juanmamaffei@bitbucket.com>
2018-04-05 10:20:50 -0300

    Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180316200025) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cards", force: :cascade do |t|
     t.integer "company_id"
     t.integer "user"
